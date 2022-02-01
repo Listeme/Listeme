66c81a53ae8f3b0137e25a9849f1eafe1c829526
Rahul <64806401+fangyman@users.noreply.github.com>
2022-01-31 19:49:01 -0500

    Used flex container to make buttons

--- client/src/Landing_Page/landing_page.js
+++ client/src/Landing_Page/landing_page.js
@@ -1,18 +1,16 @@
-import React, { Component }  from 'react';
+import React, { Component } from "react";
 import "./landing_page.css";
 
 export default class LandingPage extends Component {
-
-    render(){
-        return(
-            <div>
-                <h1>Listeme</h1>
-                <li className="login">Login</li>
-                <li className="signup">Signup</li>
-            </div>
-        );
-    };
-        
-
+	render() {
+		return (
+			<div>
+				<ul class="flex-container">
+					
+                    <li className="first">Login</li>
+                    <li>Signup</li>
+				</ul>
+			</div>
+		);
+	}
 }
-

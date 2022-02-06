ac841a35045573d3b9065cc557d5f25e0c6d74a2
Peter Ghobrial <peter_ghob@yahoo.ca>
2022-02-05 22:10:22 -0500

    Add checkbox to tasks

--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -7,6 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
 
 function createData(name, due, notes) {
     return { name, due, notes };
@@ -25,7 +26,7 @@ function createData(name, due, notes) {
             Taskbar goes here
         </div>
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650, bgcolor: 'primary.main' }} aria-label="simple table">
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell>Task Name</TableCell>
@@ -37,11 +38,9 @@ function createData(name, due, notes) {
             {rows.map((row) => (
               <TableRow
                 key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 }, bgcolor: 'primary.main' }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >           
+                <TableCell component="th" scope="row"><Checkbox color="success"/>{row.name}</TableCell>
                 <TableCell align="right">{row.due}</TableCell>
                 <TableCell align="right">{row.notes}</TableCell>
               </TableRow>

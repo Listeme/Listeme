0b78a09ac2b4f4c754ce509fe22688fc9bc73207
Peter Ghobrial <peter_ghob@yahoo.ca>
2022-02-10 18:19:53 -0500

    Reorganize into components

--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -9,9 +9,8 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
-import { useFormControl } from '@mui/material/FormControl';
 
-function createData(name, due, notes) {
+  function createData(name, due, notes) {
     return { name, due, notes };
   }
   
@@ -20,56 +19,126 @@ function createData(name, due, notes) {
     createData('Eat Spaghetti', 'February 9th', 'Spaghetti Arts'),
     createData('Become Epic', 'None', 'CSC301 Lec 3'),
   ];
+
+  class TaskName extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
   
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    render() {
+      return (
+        <TextField
+          id="task_name"
+          label="Add Task"
+          defaultValue=""
+          variant="standard" />
+      );
+    }
+  }
+
+  class DueDate extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    render() {
+      return (
+        <TextField
+          id="due_date"
+          label="Due Date"
+          defaultValue=""
+          variant="standard" />
+      );
+    }
+  }
+
+  class Notes extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    render() {
+      return (
+        <TextField
+          id="notes"
+          label="Notes"
+          defaultValue=""
+          variant="standard" />
+      );
+    }
+  }
+
+  function AddTask(props) {
+    return (
+      <TableRow>
+        <TableCell component="th" scope="row"> <TaskName />
+        </TableCell>
+        <TableCell align="right"> <DueDate />
+        </TableCell>
+        <TableCell align="right"> <Notes />
+        </TableCell>
+      </TableRow>
+    );
+  }
+  
+  function Tasklist(props) {
+    return (
+      <div className='Tasks-header'>
+        <div className='Tasks-spacer'>
+          Taskbar goes here
+        </div>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="Tasklist">
+            <TableHead>
+              <TableRow>
+                <TableCell>Task Name</TableCell>
+                <TableCell align="right">Due Date</TableCell>
+                <TableCell align="right">Notes</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row"><Checkbox color="success" />{row.name}</TableCell>
+                  <TableCell align="right">{row.due}</TableCell>
+                  <TableCell align="right">{row.notes}</TableCell>
+                </TableRow>
+              ))}
+              <AddTask />
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+
+    );
+  }
+
   export default function Tasks() {
     return (
-    <div className='Tasks-header'>
-        <div className='Tasks-spacer'>
-            Taskbar goes here
-        </div>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Task Name</TableCell>
-              <TableCell align="right">Due Date</TableCell>
-              <TableCell align="right">Notes</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >           
-                <TableCell component="th" scope="row"><Checkbox color="success"/>{row.name}</TableCell>
-                <TableCell align="right">{row.due}</TableCell>
-                <TableCell align="right">{row.notes}</TableCell>
-              </TableRow>
-            ))}
-            <TableRow>
-              <TableCell component="th" scope="row"> <TextField
-                id="add_task"
-                label="Add Task"
-                defaultValue=""
-                variant="standard"/>    
-              </TableCell>
-              <TableCell align="right"> <TextField
-                id="due_date"
-                label="Due Date"
-                defaultValue=""
-                variant="standard"/>    
-              </TableCell>
-              <TableCell align="right"> <TextField
-                id="notes"
-                label="Notes"
-                defaultValue=""
-                variant="standard"/>    
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+      <Tasklist />
     );
   }

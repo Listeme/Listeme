e5a8bc1a95125e53d6f62af4d8d77a47f20971b2
Rahul <64806401+fangyman@users.noreply.github.com>
2022-02-10 18:30:43 -0500

    Changed import statements

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import LandingPage from './Pages/landing_page';
-import LoginPage from './Pages/login_page';
-import SignupPage from './Pages/signup_page';
-import ForgotPasswordPage from './Pages/forgot_password';
+import LandingPage from './pages/landing_page';
+import LoginPage from './pages/login_page';
+import SignupPage from './pages/signup_page';
+import ForgotPasswordPage from './pages/forgot_password';
 
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -15,8 +15,7 @@ function App() {
     <ChakraProvider theme={font_theme}>      
       <ToggleColorMode/>    
       <BrowserRouter>
-        <Routes>
-          
+        <Routes>          
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage/>} />

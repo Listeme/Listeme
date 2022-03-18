9ac55a36461cd5315de0d80525770ce3d17215ee
manjoy malhotra <manjoy.malhotra@mail.utoronto.ca>
2022-03-18 01:47:38 -0400

    functional calendar

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -3,18 +3,20 @@ import Calendar from 'react-calendar';
 import './Calendar.css';
 // import 'react-calendar/dist/Calendar.css';
 
+// code borrowed from online 
 function MakeCalendar() {
   const [date, setDate] = useState(new Date());
 
   return (
     <div className='app'>
+      
       <div className='calendar-container'>
         <Calendar onChange={setDate} value={date} />
       </div>
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
+      <div>
+        <span >Selected Date:</span>{' '}
         {date.toDateString()}
-      </p>
+      </div>
     </div>
   );
 }

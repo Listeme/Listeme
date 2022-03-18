b86c386612199c476089350cfc2781dac8e72cb0
David <44748556+Quietfu1@users.noreply.github.com>
2022-03-18 05:47:16 -0400

    Added skeleton query for getting notes

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,11 +1,32 @@
 import { useState } from 'react';
+import {gql, useQuery} from '@apollo/client';
+import { useNavigate } from "react-router-dom";
 import Calendar from 'react-calendar';
 import './Calendar.css';
 // import 'react-calendar/dist/Calendar.css';
 
+const GET_TASKS = gql`
+    query Tasks($where: TaskWhere) {
+      tasks(where: $where) {
+        endDate
+        name
+        user {
+          email
+        }
+      }
+    }
+`; 
+
 // code borrowed from online 
 function MakeCalendar() {
   const [date, setDate] = useState(new Date());
+
+  let navigate = useNavigate();
+
+  // const [tasks, { d }] = useQuery(GET_TASKS); 
+  // async function onClickDate(values) {
+  //   ;
+  // }
 
   return (
     <div className='app'>

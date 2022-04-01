594665db9c6eb9f1f90d6e04ae2b659d634bae09
David <44748556+Quietfu1@users.noreply.github.com>
2022-04-01 03:15:13 -0400

    [dev-019] Added query to retrieve user's tasks

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,32 +1,55 @@
-import { useState } from 'react';
-import {gql, useQuery} from '@apollo/client';
+import React, {useEffect, useState} from 'react';
+import {Box} from "@chakra-ui/react";
+import {gql, useLazyQuery} from '@apollo/client';
 import { useNavigate } from "react-router-dom";
 import Calendar from 'react-calendar';
 import './Calendar.css';
 // import 'react-calendar/dist/Calendar.css';
 
 const GET_TASKS = gql`
-    query Tasks($where: TaskWhere) {
-      tasks(where: $where) {
-        endDate
+  query Tasks {
+    tasks{
+      name
+      completed
+      endDate
+      subtasks {
         name
-        user {
-          email
-        }
+      }
+      user {
+        email
       }
     }
-`; 
+  }`;
+
+  
 
 // code borrowed from online 
 function MakeCalendar() {
+  
+  const [tasks, setTasks] = useState([]);
+  const [getTask, { loading, error, data }] = useLazyQuery(GET_TASKS);
+
+  async function taskInfo() {
+    let taskInfos = await getTask();
+    return taskInfos;
+  }
+
+  useEffect(() => {
+    getTask().then(res => {
+      // console.log(res);
+      console.log("Tasks retrieved");
+      let tasks = res.data.tasks;
+      let now = new Date();
+      console.log(now);
+      // let taskFiltered = tasks.filter(task => task.completed === false && task.endDate === now);
+      console.log(tasks);
+      setTasks(tasks);
+    });
+  }, []);
+
   const [date, setDate] = useState(new Date());
 
-  let navigate = useNavigate();
-
-  // const [tasks, { d }] = useQuery(GET_TASKS); 
-  // async function onClickDate(values) {
-  //   ;
-  // }
+  console.log(loading);
 
   return (
     <div className='app'>
@@ -38,7 +61,20 @@ function MakeCalendar() {
         <span >Selected Date:</span>{' '}
         {date.toDateString()}
       </div>
+
+      <div>
+      <h1>Tasks due today</h1>
+      {tasks.map(task => (
+        <div key={task.id}>
+          <p>{task.name}</p>
+          <p>{task.subtasks}</p>
+          <p>{task.notes}</p>
+        </div>
+      ))}
+      </div>
     </div>
+
+    
   );
 }
 
